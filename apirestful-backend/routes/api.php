106219686ba04f5a94f978eae822aef1b461f179
Rafael Fleitas <rafaelportugallaeber@gmail.com>
2026-03-15 19:59:55 -0400

    Criação do backend

---
+++ apirestful-backend/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Route::get('/users', [UserController::class, 'index']);
+// Route::post('/users', [UserController::class, 'store']);
+Route::apiResource('/users', UserController::class); //Cadastra as rotas de forma automática para o recurso UserController
